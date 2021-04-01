e17949ac3e71679fdbcc8679d371d1ada9c10e5e
Alex Snetko <007alex03@gmail.com>
2021-04-01 09:38:57 +0300

    Add project files.

---
+++ LR4.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LR4._2
+{
+    class Program
+    {
+        [DllImport("MyLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int add(int a, int b);
+        [DllImport("MyLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int substract(int a, int b);
+        [DllImport("MyLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int multiply(int a, int b);
+        [DllImport("MyLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int divide(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(4, 3));
+            Console.WriteLine(substract(5, 7));
+            Console.WriteLine(multiply(2, 3));
+            Console.WriteLine(divide(4, 6));
+        }
+    }
+}
